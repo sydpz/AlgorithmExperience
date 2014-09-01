b2a586d070135547a60860a1bf1f57c0a15deae3
bingwang <wbing1987@yahoo-inc.com>
2014-09-01 13:49:55 +0800

    add solution symmetricTree

---
+++ AlgorithmExperience/src/SymmetricTree/Solution.java
@@ -0,0 +1,29 @@
+package SymmetricTree;
+
+import DataStructure.TreeNode;
+
+public class Solution {
+	public boolean isSymmetric(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+
+		return isSymmetric(root.left, root.right);
+
+	}
+
+	private boolean isSymmetric(TreeNode left, TreeNode right) {
+		if (left == null && right == null) {
+			return true;
+		} else if (left == null && right != null) {
+			return false;
+		} else if (left != null && right == null) {
+			return false;
+		} else if (left.val != right.val) {
+			return false;
+		}
+
+		return isSymmetric(left.left, right.right)
+				&& isSymmetric(left.right, right.left);
+	}
+}
